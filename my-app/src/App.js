58a428a70d6bda4c0b63c3ca804595de1d0b485f
DanielJMus <danielmus1999@hotmail.com>
2019-08-21 09:08:03 +1200

    Initial App Setup

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-content">
+        <h3 className="App-title">Coming Soon</h3>
+        <h1 className="App-title">Portfolio Site</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
